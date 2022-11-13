2793cec39a310f4db2a1781f202da80619743523
vtrtga <vitor.ypvalim@gmail.com>
2022-11-13 15:57:56 -0400

    parte-3

--- exercicios-entrada-de-dados/src/Main.java
+++ exercicios-entrada-de-dados/src/Main.java
@@ -8,23 +8,17 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String x;
+		String s1, s2, s3;
 		
-		int y;
-		
-		double z;
-		
-		x = sc.next();
-		
-		y = sc.nextInt();
-		
-		z = sc.nextDouble();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
 		
 		System.out.println("Dados digitados: ");
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-	
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
 		sc.close();
 	}
 
