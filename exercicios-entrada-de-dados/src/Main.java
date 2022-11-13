0be6eda4493a805b6f44729b1c4269ee287cf760
vtrtga <vitor.ypvalim@gmail.com>
2022-11-13 15:52:46 -0400

    system parte 2

--- exercicios-entrada-de-dados/src/Main.java
+++ exercicios-entrada-de-dados/src/Main.java
@@ -1,14 +1,30 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
 		
-		double x;
+		String x;
 		
-		x = sc.nextDouble();
-		System.out.println("Voce digitou: " + x);
+		int y;
+		
+		double z;
+		
+		x = sc.next();
+		
+		y = sc.nextInt();
+		
+		z = sc.nextDouble();
+		
+		System.out.println("Dados digitados: ");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	
 		sc.close();
 	}
 
