211652cff2d7385374d05236906caa28271272e1
vtrtga <vitor.ypvalim@gmail.com>
2022-11-14 14:21:02 -0400

    ex1/pdf

--- exercicios-entrada-de-dados/src/Main.java
+++ exercicios-entrada-de-dados/src/Main.java
@@ -1,25 +1,20 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		
 		Scanner sc = new Scanner(System.in);
 		
-		String s1, s2, s3;
+		int A, B, soma;
 		
-		s1 = sc.nextLine();
-		s2 = sc.nextLine();
-		s3 = sc.nextLine();
+		System.out.println("Digite um número: ");
+		A = sc.nextInt();
+		System.out.println("Digite outro número: ");
+		B = sc.nextInt();
 		
-		System.out.println("Dados digitados: ");
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-
-		sc.close();
+		soma = A + B;
+		
+		System.out.println("SOMA = " + soma);
 	}
 
 }
